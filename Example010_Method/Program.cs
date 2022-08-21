d593158ac73a7c2e89a2caf7f9ef84ef9430660b
Sithvertex <ibusu@yandex.ru>
2022-08-21 17:33:55 +0300

    ADD Example010_Method Array

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = { 1, 12, 13, 14, 5, 14, 65, 117 };
+
+int n = array.Length;
+
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
