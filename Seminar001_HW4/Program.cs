d2533e4df05d835c92df4db0b2b034b60f41bf3c
Sithvertex <ibusu@yandex.ru>
2022-08-21 23:32:47 +0300

    add show all the numbers are even

---
+++ Seminar001_HW4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите любое число  ");
+int input = int.Parse(Console.ReadLine());
+
+int i, j, fk, b, s;
+i = 0;
+j = 0;
+fk = 0;
+while ( i < input)
+{
+    i++;
+    if (i %2 == 0)
+    {
+        j++;
+      //  Console.WriteLine(j); 
+    }
+}
+b = 0;
+while (b < j)
+{ 
+    b++;
+    s = 2*b;
+    Console.WriteLine(s);    
+}
