23eb7e136ab9ac7f4e984f69a3b586098162a929
Sithvertex <ibusu@yandex.ru>
2022-08-21 20:46:41 +0300

    ADD HW_2 MAX from 3

---
+++ Seminar001_HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+Console.Write("Введите любое число 1 ");
+int input1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число 2 ");
+int input2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число 3 ");
+int input3 = Convert.ToInt32(Console.ReadLine());
+
+int max = Max(input1, input2, input3);
+
+Console.Write("Макимальное число:" +max);
