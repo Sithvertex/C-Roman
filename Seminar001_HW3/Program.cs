9f8256130fdde5b23e7c56fa49d554189e9b49dc
Sithvertex <ibusu@yandex.ru>
2022-08-21 22:41:12 +0300

    add ВЫвод четное или нет

---
+++ Seminar001_HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите любое число  ");
+int input = int.Parse(Console.ReadLine());
+
+if ( input != 0)
+{
+    if (input %2 == 0)
+    {
+       Console.WriteLine("Число четное");      
+    }
+    else
+    {
+       Console.Write("Число не четное");
+    }  
+}
+else
+{
+    Console.Write("Число ноль");
+}
+ 
