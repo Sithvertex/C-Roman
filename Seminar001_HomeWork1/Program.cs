b60ca5d05178fb37919eba4a8b35e3622ad9fe28
Sithvertex <ibusu@yandex.ru>
2022-08-21 20:03:03 +0300

    add HomeWORK_1, another HW_1 Seminar 1

---
+++ Seminar001_HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿//создание переменных
+double a1;
+double a2;
+string input1;
+string input2;
+double max;
+Console.Write("Введите любое вещественое число 1 ");
+input1 = Console.ReadLine();
+Console.Write("Введите любое вещественое число 2 ");
+input2 = Console.ReadLine();
+
+a1 = Double.Parse(input1);
+a2 = Double.Parse(input2);
+
+max = a1;
+if( a2 > max ) max = a2;
+
+Console.Write("Макимальное число:" +max);
+// Вопрос на семинаре, как считать с консоли число, без
+// конвертации:
+//int input = Console.Read();
