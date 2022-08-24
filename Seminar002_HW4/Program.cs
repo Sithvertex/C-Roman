a65694507e8e9a042813596a5add5681a5f3264d
Roman <ibusu@yandex.ru>
2022-08-25 00:53:11 +0300

    ADD 7 and 23 == 0

---
+++ Seminar002_HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число  ");
+int input = int.Parse(Console.ReadLine());
+
+if (input % 7 ==0 && input % 23 == 0)
+{
+Console.Write("Число кратно 7 и 23 одновремено:");
+}
+else
+{
+Console.Write("Число не кратно 7 и 23 одновремено");
+}
+// Проверить можно числом 16905
