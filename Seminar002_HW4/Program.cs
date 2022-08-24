6b0aab44848d6d569a48e08677d89c54f74261f3
Roman <ibusu@yandex.ru>
2022-08-25 01:01:51 +0300

    ADD 7&&23

--- Seminar002_HW4/Program.cs
+++ Seminar002_HW4/Program.cs
@@ -1,12 +1,16 @@
-﻿Console.Write("Введите число  ");
-int input = int.Parse(Console.ReadLine());
+﻿int start = 1;
+int stop = 1000000;
 
+//2.Генерация числа из условий (1 1000000]
+int input = new Random().Next(start,stop+1);
+
+//Проверка условий и вывод
 if (input % 7 ==0 && input % 23 == 0)
 {
-Console.Write("Число кратно 7 и 23 одновремено:");
+Console.Write("Число кратно 7 и 23 одновремено: число " +input);
 }
 else
 {
-Console.Write("Число не кратно 7 и 23 одновремено");
+Console.Write("Число не кратно 7 и 23 одновремено: число " +input);
 }
 // Проверить можно числом 16905
