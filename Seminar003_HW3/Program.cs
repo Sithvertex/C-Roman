b3f1c22f4837a572e802e8c3fc9e3b0c67385e89
Roman <ibusu@yandex.ru>
2022-08-26 21:42:38 +0300

    ADD Cube N^3

---
+++ Seminar003_HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Введение исходных данных
+Console.Write("Введите любое число  ");
+int input = int.Parse(Console.ReadLine());
+
+// Определение временых переменых
+int s, k, f;
+f = 0;
+s = 0;
+
+//Определение массива и его разменрности
+// +1 потому что есть a[0] поэтому на 1 больше
+int[] a = new int[input+1];
+
+// формирование таблицы кубов
+for (int j = 0; j <= input; j++)
+{        
+    f = f + j;
+    s = Convert.ToInt32(Math.Pow(f, 3)); 
+    a[j] = s;
+    Console.WriteLine(a[j]); 
+    s = 0;
+    f = 0;    
+}
