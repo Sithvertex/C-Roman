2b3536e49e4ded6eb85eb67e9f35b9a54538fc44
Roman <ibusu@yandex.ru>
2022-08-27 14:55:01 +0300

    ADD ПАЛИНДРОМ числа :wq

--- Seminar003_HW1/Program.cs
+++ Seminar003_HW1/Program.cs
@@ -1,50 +1,62 @@
-﻿// Введение исходных данных
-Console.Write("Введите любое число  ");
-int input = int.Parse(Console.ReadLine());
+﻿// Напишите программу, которая принимает на 
+// вход пятизначное число(ЗДЕСЬ я изменил для любого
+// натуральног числа. Да простят меня Боги!!) и проверяет,
+// является ли оно палиндромом.
 
-//b[i] = int.Parse(str[i].ToString());
+// Введение исходных данных
+Console.Write("Введите любое натуральное число  ");
+string input = Console.ReadLine();
+int length = input.Length;
+int number = int.Parse(input);
 
+// Задаем массив и переменые
+int[] a = new int[length];
+int[] b = new int[length];
+int s, number_1,number_2,k,l;
+s = 0;
 
-//int[] b = new int[str.Length];
-
-
-//Console.WriteLine(b);
-
-int i, v, fer, value;
-//double c;
-//double value;
-//int[] a = new int[8];
-
-value =10;
-i = 0;
-//v = input;
-
-do
+// Разбиваем введеное число на числа и помещаем каждое
+// в элемент массива и создаем обратный массив (для будущей
+// проверки)
+for (int i = 0; i < length; i++)
 {
-    value = input / 10;
-    fer=  input % 10;    
-   // a[i] = value;
-    input = fer;
-    i++;
-    //Console.WriteLine("Трех-значное число:" +value);
-    Console.WriteLine("Трех-значное число:" +i);
+    s = Convert.ToInt32(Math.Pow(10, ((length-i)-1)));
+    number_1 = number / s;
+    number_2 = number % s;
+    a[i] = number_1;
+    k = (length-i)-1;
+    b[k] = number_1;
+    number = number_2;
 }
-while (value > 9);
+//Функция типа 4 для вывода массивов
+void PrintArray(int[] array)
+{
+    int count = array.Length;
 
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+//PrintArray(a);
+//PrintArray(b);
 
-
-
-
-//while ( value > 9)
-//{
-  //  Console.WriteLine(i);
- //   s = Convert.ToInt32(Math.Pow(10, i)); 
-  //  value = input / 10;
-  //  fer=  input % 10;    
-   // a[i] = value;
- //   input = fer;
-  //  i++;
-    //Console.WriteLine("Трех-значное число:" +value);
-  ///  Console.WriteLine("Трех-значное число:" +i);
-    
-//}
+l = 0;
+for (int i = 0; i < length; i++)
+{
+    if ( a[i]== b[i])
+    {
+        l++;
+        if (l == length)
+        {
+        Console.Write("Число является палиндромом");
+        }
+    }
+}
+if (l != length)
+{
+Console.Write("Число не является палиндромом");
+}
+// С выводом наверно можно было и проще, но 
+//у меня времени просто не хватила :)
