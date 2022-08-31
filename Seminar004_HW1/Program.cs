5fa4ca16e521d62d1314542f5e7e963814294b51
Roman <ibusu@yandex.ru>
2022-08-31 15:47:47 +0300

    add exponentiation of a number

--- Seminar004_HW1/Program.cs
+++ Seminar004_HW1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+//  Не использовать функцию Math.Pow()
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A=");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень B=");
+int b = Int32.Parse(Console.ReadLine());
+
+int tmp;
+tmp= 1;
+
+for  (int i = 0; i < b; i++)
+{
+    tmp*=a;
+}
+
+Console.WriteLine(tmp);
