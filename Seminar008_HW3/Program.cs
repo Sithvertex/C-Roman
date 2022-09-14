a088c2a336d995447019161782f7f51d01a6057c
Roman <ibusu@yandex.rugit commit -agit config user.email ibusu@yandex.ru>
2022-09-14 17:35:23 +0300

    add non-repeating elements in the array

---
+++ Seminar008_HW3/Program.cs
@@ -0,0 +1,88 @@
+﻿// Задача 3. Сформируйте двухмерный массив из неповторяющихся
+// двузначных чисел (размер массива не более 50 элементов). 
+// Напишите программу, которая будет построчно выводить массив.
+// Вариант создания функции: Проверить число на присутствие в массиве 
+// (bool Contains(int[,] arr, int value){...} )
+// Пример:
+// Массив размером 3 x 3
+// 10 11 55
+// 33 41 23
+// 17 28 34
+
+string t = "";
+Console.WriteLine("Введите размерность массива n*m (через пробел):");
+t = Console.ReadLine();
+string[] tv = t.Split(' ').ToArray();
+int n = int.Parse(tv[0]);
+int m = int.Parse(tv[1]);
+
+int start = 1;
+int stop = 10;
+int[,] a = new int[n, m];
+
+//Функция заполнения масива двухмерного масива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(start, stop + 1);
+        }
+    }
+}
+
+FillArray(a);
+
+//Функция для проверки есть ли это число в двухмерном массиве
+bool IsinArray(int[,] array, int value)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] == value)
+            {
+                return true;
+            }
+        return false;
+    }
+}
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        tmp = a[i, j];
+        IsinArray(a, tmp);
+        if (IsinArray(a, tmp) == true)
+        {
+            int tmp = new Random().Next(start, stop + 1);
+        }
+        else
+        {
+            int tmp = 0;
+        }
+
+    }
+}
+
+//Функция для вывода двухмерного массива
+void PrintArray2(int[,] array)
+{
+    int count1 = array.GetLength(0);
+    int count2 = array.GetLength(1);
+
+    for (int i = 0; i < count1; i++)
+    {
+        for (int j = 0; j < count2; j++)
+        {
+            Console.Write($" {array[i, j]},");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// Console.WriteLine("Итоговая матрица перемножения");
+PrintArray2(a);
