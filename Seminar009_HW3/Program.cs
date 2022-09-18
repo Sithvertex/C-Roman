73ed34ee2a0cd5a04a34180ff0826be2b75da914
Sithvertex <ibusu@yandex.ru>
2022-09-18 12:05:28 +0300

    add function Ankerman

---
+++ Seminar009_HW3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3. Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+// Например
+// m = 2, n = 3 -> A(m,n) = 29
+
+string t = "";
+Console.WriteLine("Введите размерность массива m*n (через пробел):");
+t = Console.ReadLine();
+string[] tv = t.Split(' ').ToArray();
+int m = int.Parse(tv[0]);
+int n = int.Parse(tv[1]);
+
+int ackerman(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if ((a > 0) && (b == 0))
+    {
+        return ackerman(a - 1, 1);
+    }
+    else if ((a > 0) && (b > 0))
+    {
+        return ackerman(a - 1, ackerman(a, b - 1));
+    }
+    else
+        return b + 1;
+}
+
+Console.WriteLine("Значение функции Аккермана = {0}", ackerman(m, n));
