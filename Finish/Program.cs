27833060e52d40b8d1017fe47a96b5c1fe0ca33b
Sithvertex <ibusu@yandex.ru>
2022-10-04 17:10:14 +0300

    add Finish work array string :

---
+++ Finish/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+// меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+// Примеры:
+// ["Hello", "2", "world", ":-)" > ["2", ":-)*]
+// ['1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string t = "";
+Console.WriteLine("Введите значения элементов (через запятую):");
+t = Console.ReadLine();
+string[] tv = t.Split(',').ToArray();
+
+int m = tv.GetLength(0);
+
+string[] array1 = new string[m];
+string[] array2 = new string[m];
+
+ for (int i = 0; i < m; i++)
+{
+    array1[i] = tv[i];
+}
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
